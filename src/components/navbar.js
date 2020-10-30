b0d74901e0cc5bb5e39bf74896329456bcd743d0
warishasan <waris.hasan00@gmail.com>
2020-10-30 19:28:02 +0500

    made responsive

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,73 +1,88 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import logo from "../images/logo.png"
-
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import logo from "../images/logo.png";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    
-  },
   appBar: {
-    backgroundImage:  "linear-gradient(to bottom, lightgrey , white)"
+    backgroundImage: "linear-gradient(to bottom, lightgrey , white)",
   },
-  title: {
-   color: "black",
-   fontSize: "40px",
-   minWidth : "200px",
-   textShadow: "2px 5px 5px black",
-   marginLeft: "20px"
-  
-  },
-  logo:{
-      marginTop:"5px",
-      marginLeft: "30px",
-      height: "100px",
-      width: "100px"
-  },
-  buttonGroup:{
-      marginLeft: "900px",
-      display: "flex",
-      flexDirection: "row"
-  },
-  buttons:{
-      padding:"10px",
-      margin: "20px",
-      width : "150px",
-      fontSize: "18px",
-      backgroundColor:"black",
-      color: "white",
-      borderRadius: "15px",
-      '&:hover': {
-        backgroundColor: "darkgrey"
-    
-    },
 
-  }
+  toolbar: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flexWrap: "wrap",
+  },
+
+  typo: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  logo: {
+    marginTop: "1rem",
+    marginLeft: "1rem",
+    height: "5rem",
+    width: "5rem",
+  },
+
+  title: {
+    fontSize: "2rem",
+    color: "black",
+  },
+  buttons: {
+    padding: "1rem",
+    margin: "1rem",
+    fontSize: "1rem",
+    backgroundColor: "black",
+    color: "white",
+
+    borderRadius: "2vmax",
+    "&:hover": {
+      backgroundColor: "darkgrey",
+    },
+  },
 }));
 
 export default function Navbar() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <AppBar className={classes.appBar} position="static">
-        <Toolbar>
-        <img className = {classes.logo} src = {logo} alt = "siteLogo"></img>
-          <Typography variant="h6" className={classes.title}>
-            BIG BANG
-          </Typography>
-          <div className = {classes.buttonGroup}>
-          <button  className = {classes.buttons}>Home</button>
-          <button onClick ={()=> alert("This page has not been created yet. This is only a test site")} className = {classes.buttons}>Services</button>
-          <button onClick ={()=> alert("This page has not been created yet. This is only a test site")}  className = {classes.buttons}> About</button>
-          </div>
-          
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar className={classes.appBar} position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h6" className={classes.typo}>
+          <img className={classes.logo} src={logo} alt="siteLogo"></img>
+          <h3 className={classes.title}> BIG BANG</h3>
+        </Typography>
+
+        <Typography variant="h6" className={classes.typo}>
+          <button
+            onClick={() =>
+              alert(
+                "This page has not been created yet. This is only a test site"
+              )
+            }
+            className={classes.buttons}
+          >
+            Services
+          </button>
+          <button
+            onClick={() =>
+              alert(
+                "This page has not been created yet. This is only a test site"
+              )
+            }
+            className={classes.buttons}
+          >
+            {" "}
+            About
+          </button>
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
